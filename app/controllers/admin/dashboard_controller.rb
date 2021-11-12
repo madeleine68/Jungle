4c959677d2ea9e69bfc1bf6e6252a087ed164e4e
Madeleine <mahdieh.moghadasi@gmail.com>
2021-11-11 23:19:57 -0500

    admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['NAME'].to_s, password: ENV['PASSWORD'].to_s
 
   def show
+    @product_count = Product.count
+    @categories_count = Category.count
   end
 end
