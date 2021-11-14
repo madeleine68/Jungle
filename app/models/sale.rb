90e9d9def16be802d7d68439a5b9bf3fcad91503
Madeleine <mahdieh.moghadasi@gmail.com>
2021-11-14 14:31:50 -0500

    add sale on pages

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,12 @@
 class Sale < ActiveRecord::Base
+
+    #AR Scope
+    def self.active
+        where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+    end    
+    # another way for defining scope
+    # scope :active, -> { where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)"}
+
     def finished?
         ends_on < Date.current
     end  
