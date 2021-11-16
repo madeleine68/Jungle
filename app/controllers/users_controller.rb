4aa3ad66f0f687f591902e743498ecc954dfa52c
Madeleine <mahdieh.moghadasi@gmail.com>
2021-11-15 23:31:37 -0500

    user specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
     #rendering signup form
     def new
+        @user = User.new
     end
     #receiving the form and creating a user with the form's parameters
     def create
-        user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
             redirect_to '/'
         else
             redirect_to '/signup'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    validates :name, presence: true, length: { minimum: 3}
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true,confirmation: true, length: { minimum: 5}
+    validates :password_confirmation, presence: true
+
+    def self.authenticate_with_credentials(email, password)
+        email = email.downcase.strip
+        user = User.find_by_email(email)
+        if user && user.authenticate(password)
+          user
+        else
+          nil
+        end
+      end
 end
